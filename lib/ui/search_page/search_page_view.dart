566d92f621b8689cf68925f7caae2dc1512d158e
chenhongzhang <2639824585@qq.com>
2023-03-22 17:42:04 +0800

    补充提交

--- lib/ui/search_page/search_page_view.dart
+++ lib/ui/search_page/search_page_view.dart
@@ -1,17 +1,21 @@
+import 'package:appbar_animated/appbar_animated.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_advanced/bean/common_response.dart';
 import 'package:flutter_advanced/ui/search_page/search_page_state.dart';
 import 'package:flutter_advanced/ui/search_page/widget/autocomplete_widget.dart';
+import 'package:flutter_advanced/ui/search_page/widget/common_custom.dart';
 import 'package:get/get.dart';
 
+import '../../bean/hot_search.dart';
 import '../../generated/assets.dart';
 import 'search_page_logic.dart';
 
-class SearchPagePage extends StatefulWidget {
+class SearchPage extends StatefulWidget {
   @override
-  State<SearchPagePage> createState() => _SearchPagePageState();
+  State<SearchPage> createState() => _SearchPageState();
 }
 
-class _SearchPagePageState extends State<SearchPagePage> {
+class _SearchPageState extends State<SearchPage> {
   final logic = Get.put(SearchPageLogic());
   SearchState state = SearchState();
   @override
@@ -24,14 +28,18 @@ class _SearchPagePageState extends State<SearchPagePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Column(
+      body: Obx(() {
+        return ScaffoldLayoutBuilder(
+          backgroundColorAppBar: const ColorBuilder(Colors.transparent, Color(0xff9ad2fd)),
+          textColorAppBar: const ColorBuilder(Color(0xff4caee3),Colors.white),
+          appBarBuilder: _appBar,
+          child: SingleChildScrollView(
+              child: Column(
             children: [
               Stack(
                 children: [
                   Container(
-                    height: 200,
+                    height: 220,
                     width: MediaQuery.of(context).size.width,
                     child: Image.asset(
                       Assets.imagesIconSearchBg,
@@ -39,55 +47,155 @@ class _SearchPagePageState extends State<SearchPagePage> {
                     ),
                   ),
                   Container(
-                    height: 120,
-                    child: Row(
+                    margin: EdgeInsets.only(
+                      top: MediaQuery.of(context).size.height * 0.25,
+                    ),
+                    height: 618,
+                    decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.vertical(
+                        top: Radius.circular(30),
+                      ),
+                      color: Colors.white,
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        ClipRRect(
-                          borderRadius: BorderRadius.circular(50),
-                          child: TextButton(
-                            onPressed: () {
-                              Get.back();
-                            },
-                            child: Padding(
-                              padding: EdgeInsets.only(left: 5, right: 5),
-                              child: Image.asset(
-                                Assets.imagesIconBack,
-                                width: 30,
-                                height: 30,
-                                color: Color(0xff4daee3),
-                              ),
-                            ),
-                          ),
-                        ),
-                        Expanded(child: BuildAutoComplete())
+                        Padding(
+                            padding: EdgeInsets.only(left: 22, top: 30),
+                            child: Row(
+                              children: [
+                                Image.asset(Assets.imagesIconHot, color: Color(0xff4caee3), width: 20, height: 20),
+                                SizedBox(
+                                  width: 5,
+                                ),
+                                Text(
+                                  "热搜榜",
+                                  style: TextStyle(fontSize: 15, color: Colors.black54, fontWeight: FontWeight.bold),
+                                )
+                              ],
+                            )),
+                        Divider(height: 20),
+                        Expanded(
+                            child: state.hotSearchDetail.isEmpty
+                                ? Container()
+                                : SingleChildScrollView(
+                                    child: GridView.builder(
+                                        shrinkWrap: true,
+                                        physics: NeverScrollableScrollPhysics(),
+                                        padding: EdgeInsets.symmetric(horizontal: 16),
+                                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                                            //横轴元素个数
+                                            crossAxisCount: 2,
+                                            //纵轴间距
+                                            mainAxisSpacing: 10,
+                                            //横轴间距
+                                            crossAxisSpacing: 30.0,
+                                            //子组件宽高长度比例
+                                            childAspectRatio: 4.0),
+                                        itemCount: 20,
+                                        itemBuilder: (BuildContext context, int index) {
+                                          HotSearch item = state.hotSearchDetail[index];
+                                          return Container(
+                                            margin: EdgeInsets.only(left: 5),
+                                            child: Row(
+                                              children: [
+                                                Container(
+                                                  width: 25,
+                                                  child: Text(
+                                                    "${index + 1}",
+                                                    style: TextStyle(color: index < 3 ? Colors.red : Colors.grey),
+                                                  ),
+                                                ),
+                                                Expanded(
+                                                    child: Column(
+                                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                                  children: [
+                                                    Text(
+                                                      "${item.searchWord ?? ""}",
+                                                      style: TextStyle(fontSize: 15, color: Colors.black),
+                                                      overflow: TextOverflow.ellipsis,
+                                                    ),
+                                                    SizedBox(height: 2),
+                                                    Text(
+                                                      "${item.content ?? ""}",
+                                                      style: TextStyle(fontSize: 11, color: Colors.grey),
+                                                      overflow: TextOverflow.ellipsis,
+                                                    ),
+                                                  ],
+                                                ))
+                                              ],
+                                            ),
+                                          );
+                                        }),
+                                  )),
                       ],
                     ),
                   ),
                 ],
-              )
+              ),
             ],
+          )),
+        );
+      }),
+    );
+  }
+
+  Widget _appBar(BuildContext context, ColorAnimated colorAnimated) {
+    return Container(
+      color: colorAnimated.background,
+      padding: EdgeInsets.only(top: 30),
+      child: Row(
+        children: [
+          Container(
+            width: 50,
+            height: 50,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(100),
+            ),
+            child: TextButton(
+                onPressed: () {
+                  Get.back();
+                },
+                child: Image.asset(
+                  Assets.imagesIconBack,
+                  width: 25,
+                  height: 25,
+                  color: colorAnimated.color,
+                )),
           ),
-          Positioned(
-              top: 140,
-              child: Container(
-                width: MediaQuery.of(context).size.width,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(topLeft: Radius.circular(20.0), topRight: Radius.circular(20.0)),
-                ),
-                height: MediaQuery.of(context).size.height,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                        padding: EdgeInsets.only(left: 20, top: 15),
-                        child: Text(
-                          "热搜榜",
-                          style: TextStyle(fontSize: 14, color: Color(0xff4caee3), fontWeight: FontWeight.bold),
-                        ))
-                  ],
-                ),
-              ))
+          Expanded(
+              child: BuildAutoComplete(
+            clearText: () {
+              /// 清除内容
+            },
+            onSelected: (e) {
+              /// 选中后的回调
+            },
+            getData: (e) {
+              /// 获取填充列表的回调,返回一个列表
+              return [
+                CommonCustom("1", "郑叶"),
+                CommonCustom("2", "张云川"),
+                CommonCustom("3", "陈泓璋"),
+              ];
+            },
+          )),
+          Container(
+            width: 50,
+            height: 50,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(100),
+            ),
+            child: TextButton(
+              onPressed: () {},
+              child: Image.asset(
+                Assets.imagesIcSearch,
+                width: 25,
+                height: 25,
+                color: colorAnimated.color,
+              ),
+            ),
+          ),
         ],
       ),
     );
